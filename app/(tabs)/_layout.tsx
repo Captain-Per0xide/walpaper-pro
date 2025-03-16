384e92da1a4a814e1c970f17fe8c9e1c0274920f
Ranju Mallik <mallikranju294@gmail.com>
2025-03-16 12:05:27 +0530

    Add accounts page, remove not found screen, and update navigation structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,29 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import Feather from '@expo/vector-icons/Feather';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' ,headerShown: false}}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'For You',
+          tabBarIcon: ({ color }) => <Feather name="heart" size={24} color="black" />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <SimpleLineIcons name="feed" size={24} color="black" />,
+        }}
+      />
+            <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account-circle-outline" size={24} color="black" />,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+
+export default function Accounts(){
+    return <View>
+        <Text>Accounts Page</Text>
+    </View>
+}
